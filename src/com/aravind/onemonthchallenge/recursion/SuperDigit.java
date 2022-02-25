12f707999622a4a58b4b5940c6cabea900c06b75
topaz <topaz072021@gmail.com>
2022-02-25 22:57:13 +0530

    Add:New Problems solved

---
+++ src/com/aravind/onemonthchallenge/recursion/SuperDigit.java
@@ -0,0 +1,57 @@
+package com.aravind.onemonthchallenge.recursion;
+
+public class SuperDigit {
+
+    public static int superDigit(String n, int k)
+    {
+        String newString = n;
+        for(int i=1; i < k; i++)
+        {
+            newString = newString + n;
+        }
+
+        return (int) findRecursiveSum(newString);
+
+    }
+
+    public static long findRecursiveSum(String n)
+    {
+        long sum =0;
+        for(int i=0; i < n.length();i++)
+        {
+            sum += Integer.parseInt(n.charAt(i) + "");
+        }
+
+        if(sum/10 > 0)
+            return findRecursiveSum(sum + "");
+        return sum;
+    }
+
+    /*
+    public static long findRecursiveSum(long n)
+    {
+        long sum = 0;
+
+        while(n != 0) {
+            sum += n % 10;
+            if(n/10 > 0)
+                n = n/10;
+            else
+            {
+                n=0;
+            }
+        }
+        if(sum/10 > 0 )
+            return findRecursiveSum(sum);
+        return sum;
+    }
+    */
+
+
+
+    public static void main(String[] args)
+    {
+        int result = superDigit("148",3);
+        System.out.println(result);
+    }
+}
